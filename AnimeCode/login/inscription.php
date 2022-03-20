9d8a3270953f8cbf225681b6c0dfba50ef393a8e
Erwinn-Developpement <76838949+Erwinn-Developpement@users.noreply.github.com>
2022-03-20 09:18:21 +0100

    Add files via upload

---
+++ AnimeCode/login/inscription.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+    <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+            <link href="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/magnific-popup.min.css" rel="stylesheet" />
+            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+            <link rel="stylesheet" href="../views/texte.css">
+            <title>Inscription</title>
+        </head>
+        <body>
+        <div class="login-form">
+            <?php 
+                if(isset($_GET['reg_err']))
+                {
+                    $err = htmlspecialchars($_GET['reg_err']);
+
+                    switch($err)
+                    {
+                        case 'success':
+                        ?>
+                            <div class="alert alert-success">
+                                <strong>Succès</strong> inscription réussie !
+                            </div>
+                        <?php
+                        break;
+
+                        case 'password':
+                        ?>
+                            <div class="alert alert-danger">
+                                <strong>Erreur</strong> mot de passe différent
+                            </div>
+                        <?php
+                        break;
+
+                        case 'email':
+                        ?>
+                            <div class="alert alert-danger">
+                                <strong>Erreur</strong> email non valide
+                            </div>
+                        <?php
+                        break;
+
+                        case 'email_length':
+                        ?>
+                            <div class="alert alert-danger">
+                                <strong>Erreur</strong> email trop long
+                            </div>
+                        <?php 
+                        break;
+
+                        case 'pseudo_length':
+                        ?>
+                            <div class="alert alert-danger">
+                                <strong>Erreur</strong> pseudo trop long
+                            </div>
+                        <?php 
+                        case 'already':
+                        ?>
+                            <div class="alert alert-danger">
+                                <strong>Erreur</strong> compte deja existant
+                            </div>
+                        <?php 
+
+                    }
+                }
+                ?>
+            
+            <form action="inscription_traitement.php" method="post">
+                <h2 class="text-center">Inscription</h2>       
+                <div class="form-group">
+                    <input type="text" name="pseudo" class="form-control" placeholder="Pseudo" required="required" autocomplete="off">
+                </div>
+                <div class="form-group">
+                    <input type="email" name="email" class="form-control" placeholder="Email" required="required" autocomplete="off">
+                </div>
+                <div class="form-group">
+                    <input type="password" name="password" class="form-control" placeholder="Mot de passe" required="required" autocomplete="off">
+                </div>
+                <div class="form-group">
+                    <input type="password" name="password_retype" class="form-control" placeholder="Confirmer le mot de passe" required="required" autocomplete="off">
+                </div>
+                <div class="">
+                    <button type="submit" class="btn">Inscription</button>
+                </div>   
+
+
+            </form>
+
+      
+        </div>
+        <style>
+
+            body {
+                background: #222;
+            }
+
+            .login-form {
+                width: 540px;
+                margin: 10% auto;
+                text-align: center;
+            }
+            .login-form form {
+                margin-bottom: 15px;
+                background: #0066cc;
+                box-shadow: 0px 2px 2px #320E3B;
+                padding: 30px;
+            }
+            .login-form h2 {
+                margin: 0 0 15px;
+            }
+            .form-control, .btn {
+                min-height: 38px;
+                border-radius: 2px;
+            }
+            .btn {        
+                font-size: 15px;
+                font-weight: bold;
+                width: 250px;
+                background: #0a3c6e;
+                margin-left: auto;
+                margin-right: auto;
+            }
+        </style>
+        </body>
+</html>

0ad29b11535581a9b93b796eb37ac2fa0e537706
Erwinn-Developpement <76838949+Erwinn-Developpement@users.noreply.github.com>
2022-03-20 09:15:15 +0100

    Delete login directory

--- login/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-    try
-    {
-        $bdd = new PDO('mysql:host=localhost;dbname=login;charset=utf8', 'root', '');
-    }catch(Exception $e)
-    {
-        die('Erreur' .$e->getMessage());
-    }
